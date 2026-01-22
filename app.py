3050e5a8626160b1691029450c21e868f72aa36d
jumbo-san <jozwiakkenny0725@gmail.com>
2026-01-22 12:37:44 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET", "POST"])
+def index():
+    return render_template("index.html")
+
+# ★ ここが重要
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 # -------------------------
 # BMI 計算と体格分類
 # -------------------------
